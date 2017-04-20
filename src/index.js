18bd614a2b0582103458ad95848a62a6ffbe651f
tetiana sorochynska <tsorochynska2113@gmail.com>
2017-04-20 10:15:53 -0400

    create functions areIntersected & filterVisible

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
-﻿
+﻿'use strict';
+var rectA = {
+	left: 0,
+	top: 0,
+	width: 20,
+	height: 20
+};
+var rectB = {
+	left: -20,
+	top: 10,
+	width: 5,
+	height: 5
+};
+function areIntersected(rectA, rectB) {
+	if 	(rectA.left >= (rectB.left + rectB.width)
+		|| (rectA.left + rectA.width) <= rectB.left
+		|| rectA.top >= (rectB.top + rectB.hight)
+		|| (rectA.top + rectA.hight) <= rectB.top)
+	{ return false }
+
+	else { return true; }
+};
+
+	var array = [
+		{
+			left: 10, top: 10,
+			width: 25, height: 25
+		},
+		{
+			left: 15, top: 10,
+			width: 20, height: 20
+		},
+		{
+			left: 10, top: 10,
+			width: 0, height: 25
+		},
+		{
+			left: 100, top: 10,
+			width: 5, height: 5
+		}
+	];
+
+
+	function filterVisible(rectA, array) {
+		return array.filter(function (index) {
+			return areIntersected(rectA, index)
+			if (index.length !== 0 ) 
+			{ return true }
+			else { return false; }
+		}
+		)
+	}
+
+
+ 
+			
+
+	 
+
+
+	
+ 
+
+
