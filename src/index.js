f82b429a3604a542f35c54b3c32b4a520e712928
tetiana sorochynska <tsorochynska2113@gmail.com>
2017-04-30 09:43:15 -0400

    change conditions for function filterVisible and correct mistake in function areIntersected.

--- src/index.js
+++ src/index.js
@@ -1,64 +1,68 @@
 ﻿'use strict';
-var rectA = {
+let rectA = {
 	left: 0,
 	top: 0,
 	width: 20,
 	height: 20
 };
-var rectB = {
+let rectB = {
 	left: -20,
 	top: 10,
 	width: 5,
 	height: 5
 };
-function areIntersected(rectA, rectB) {
-	if 	(rectA.left >= (rectB.left + rectB.width)
-		|| (rectA.left + rectA.width) <= rectB.left
-		|| rectA.top >= (rectB.top + rectB.hight)
-		|| (rectA.top + rectA.hight) <= rectB.top)
-	{ return false }
 
-	else { return true; }
+function areIntersected(rectA, rectB) {
+	if (rectA.top >= (rectB.top + rectB.height)
+		|| (rectA.top + rectA.height) <= rectB.top
+		|| rectA.left >= (rectB.left + rectB.width)
+		|| (rectA.left + rectA.width) <= rectB.left) {
+		return false;
+	}
+	else {
+		return true;
+	}
 };
 
-	var array = [
-		{
-			left: 10, top: 10,
-			width: 25, height: 25
-		},
-		{
-			left: 15, top: 10,
-			width: 20, height: 20
-		},
-		{
-			left: 10, top: 10,
-			width: 0, height: 25
-		},
-		{
-			left: 100, top: 10,
-			width: 5, height: 5
-		}
-	];
-
-
-	function filterVisible(rectA, array) {
-		return array.filter(function (index) {
-			return areIntersected(rectA, index)
-			if (index.length !== 0 ) 
-			{ return true }
-			else { return false; }
-		}
-		)
+let array = [
+	{
+		left: 10, top: 10,
+		width: 25, height: 25
+	},
+	{
+		left: 15, top: 10,
+		width: 20, height: 20
+	},
+	{
+		left: 10, top: 10,
+		width: 0, height: 25
+	},
+	{
+		left: 100, top: 10,
+		width: 5, height: 5
 	}
+];
 
 
- 
-			
+function filterVisible(rectA, array) {
+	return array.filter(function (index) {
+		if (index.width !== 0 && index.height !== 0) {
+			return areIntersected(rectA, index);
+		}
+		else {
+			return false;
+		}
+	});
+}
 
-	 
 
 
-	
- 
+
+
+
+
+
+
+
 
 
